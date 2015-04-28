1a4cef84d1535abd625ee9878476d69bd06f2133
Ryan Pliske <ryanpliske@yahoo.com>
2015-04-28 13:15:05 -0500

    UI Initial Setup
    *Added FXForms
    *Cleaned Up Some Storyboard warnings

--- Tracker/SettingsViewController.swift
+++ Tracker/SettingsViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class SettingsViewController: UIViewController {
+class SettingsViewController: FXFormViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.setNeedsStatusBarAppearanceUpdate()
+        // self.view.frame.size.height = 450
+        self.view.backgroundColor = UIColor.blackColor()
+        self.tableView.backgroundColor = UIColor.blackColor()
+    }
+    
+    override func awakeFromNib() {
+        
+        formController.form = settingsForm()
     }
     
     override func viewWillAppear(animated: Bool) {
@@ -21,6 +29,7 @@ class SettingsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
     
     override func preferredStatusBarStyle() -> UIStatusBarStyle {
         return UIStatusBarStyle.LightContent;
---
+++ Tracker/settingsForm.swift
@@ -0,0 +1,30 @@
+//
+//  settingsForm.swift
+//  Tracker
+//
+//  Created by Ryan Pliske on 2/8/15.
+//  Copyright (c) 2015 Tracker. All rights reserved.
+//
+
+import UIKit
+
+class settingsForm: NSObject, FXForm {
+    var email: String?
+    var password: String?
+    var repeatPassword: String?
+    
+    func emailField()->NSDictionary{
+        return ["contentView.backgroundColor" : UIColor.darkGrayColor(),
+            "textLabel.color" : UIColor.whiteColor() ]
+    }
+    
+    func passwordField()->NSDictionary{
+        return ["contentView.backgroundColor" : UIColor.darkGrayColor(),
+            "textLabel.color" : UIColor.whiteColor() ]
+    }
+    
+    func repeatPasswordField()->NSDictionary{
+        return ["contentView.backgroundColor" : UIColor.darkGrayColor(),
+            "textLabel.color" : UIColor.whiteColor() ]
+    }
+}
