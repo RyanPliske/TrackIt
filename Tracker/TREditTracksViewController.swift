15d792dc24ce042d83915239c2ebf97097ef88e7
@ryanpliske <ryanpliske@yahoo.com>
2015-08-26 19:33:29 -0500

    Added SearchBar to EditVC

--- Tracker/TREditTracksViewController.swift
+++ Tracker/TREditTracksViewController.swift
@@ -9,21 +9,38 @@ class TREditTracksViewController: UIViewController, UITableViewDataSource {
     var trackerModel: TRTrackerModel?
     @IBOutlet weak var editTracksTableView: TREditTracksTableView!
     @IBOutlet weak var itemTypeSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var recordSearchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         editTracksTableView.dataSource = self
-        title = "Edit"
+        title = "Tracker"
         navigationItem.rightBarButtonItem = self.editButtonItem()
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: UIBarButtonItemStyle.Done, target: self, action: "dismissEditTracks")
-        navigationController?.navigationBar.barTintColor = UIColor.clearColor()
-        navigationController?.navigationBar.barStyle = UIBarStyle.BlackTranslucent
-
+        if let navController = navigationController {
+            navController.navigationBar.barStyle = UIBarStyle.BlackTranslucent
+            navController.navigationBar.barTintColor = UIColor.clearColor()
+            let navBarHairLineImageView = findHairLineImageViewUnder(navController.navigationBar)
+            navBarHairLineImageView?.hidden = true
+        }
     }
     
     override func setEditing(editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
         editTracksTableView.setEditing(editing, animated: animated)
+    }
+    
+    func findHairLineImageViewUnder(view: UIView) -> UIImageView? {
+        if ((view.isKindOfClass(UIImageView.self)) && (view.bounds.size.height <= 1.0)) {
+            return view as? UIImageView
+        }
+        for subview in view.subviews {
+            let imageView = findHairLineImageViewUnder(subview)
+            if (imageView != nil) {
+                return imageView
+            }
+        }
+        return nil
     }
     
     @IBAction func segmentControlPressed(sender: AnyObject) {
