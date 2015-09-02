95399d89a66d3675b655065bc93caeb73f4da26b
@ryanpliske <ryanpliske@yahoo.com>
2015-09-01 21:33:53 -0500

    25: Settings Table View is White Smoke Black

--- Tracker/TRSettingsTableView.swift
+++ Tracker/TRSettingsTableView.swift
@@ -2,4 +2,8 @@ import Foundation
 
 class TRSettingsTableView: UITableView {
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        backgroundColor = UIColor(red: (10.0 / 255.0), green: (10.0 / 255.0), blue: (10.0 / 255.0), alpha: 1.0)
+    }
 }
