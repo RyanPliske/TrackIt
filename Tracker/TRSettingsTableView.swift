845e4baec1b2c3a37ad73af068a9d0a287880453
@ryanpliske <ryanpliske@yahoo.com>
2015-09-02 19:24:06 -0500

    25: Fixed some frame calculations. Also set the frame within layoutSubviews instead of in the init.

--- Tracker/TRSettingsTableView.swift
+++ Tracker/TRSettingsTableView.swift
@@ -2,25 +2,20 @@ import Foundation
 
 class TRSettingsTableView: UITableView {
     
+    let topBorder = UIView()
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        let topBorder = UIView(frame: CGRectMake(self.bounds.minX,
-            self.bounds.minY,
-            self.bounds.width,
-            1.0))
+        backgroundColor = UIColor.clearColor()
         topBorder.backgroundColor = UIColor.darkGrayColor()
         addSubview(topBorder)
-        
-        let bottomBorder = UIView(frame: CGRectMake(self.bounds.minX,
-            (self.bounds.maxY - 1.0),
-            self.bounds.width,
-            1.0))
-        bottomBorder.backgroundColor = UIColor.darkGrayColor()
-        addSubview(bottomBorder)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        backgroundColor = UIColor.clearColor()
+        topBorder.frame = CGRectMake(self.bounds.minX,
+            self.bounds.minY,
+            self.bounds.width,
+            1.0)
     }
 }
--- Tracker/TRSettingsTableViewCell.swift
+++ Tracker/TRSettingsTableViewCell.swift
@@ -3,24 +3,31 @@ import Foundation
 class TRSettingsTableViewCell: UITableViewCell {
     
     @IBOutlet weak private var settingNameLabel: UILabel!
+    let bottomBorder = UIView()
     
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         selectedBackgroundView = UIView(frame: self.bounds)
-        selectedBackgroundView?.backgroundColor = UIColor(red: (20.0 / 255.0),
+        backgroundColor = UIColor(red: (20.0 / 255.0),
             green: (20.0 / 255.0),
             blue: (20.0 / 255.0),
             alpha: 1.0)
-        backgroundColor = UIColor(red: (10.0 / 255.0),
-            green: (10.0 / 255.0),
-            blue: (10.0 / 255.0),
+        selectedBackgroundView?.backgroundColor = UIColor(red: (40.0 / 255.0),
+            green: (40.0 / 255.0),
+            blue: (40.0 / 255.0),
             alpha: 1.0)
-        let bottomBorder = UIView(frame: CGRectMake(self.bounds.minX + 15.0,
-            (self.bounds.maxY - 0.5),
-            (self.bounds.width - 30.0),
-            0.5))
+
         bottomBorder.backgroundColor = UIColor.darkGrayColor()
         addSubview(bottomBorder)
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        bottomBorder.frame = CGRectMake(
+            (self.bounds.minX + self.bounds.width / 15.0),
+            (self.bounds.maxY - 0.5),
+            (self.bounds.width * (13.0 / 15.0)),
+            0.5)
     }
     
     func setSettingNameWith(name: String) {
