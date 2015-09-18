75f0671e31025992f712ebec49ef589ce1a916b3
@ryanpliske <ryanpliske@yahoo.com>
2015-09-17 20:48:04 -0500

    52: Abstracted some model and table view logic from the ManageViewController into a separate object.

--- Tracker/TRManageItemsViewController.swift
+++ Tracker/TRManageItemsViewController.swift
@@ -1,14 +1,11 @@
 import Foundation
 
-class TRManageItemsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, TRManageItemsTableViewCellDelegate {
+class TRManageItemsViewController: UIViewController, UITableViewDelegate {
     
     @IBOutlet weak var itemsTableView: UITableView!
     private let itemsModel = TRItemsModel.sharedInstanceOfItemsModel
+    private var itemsPresenter: TRManageItemsPresenter?
     private var selectedRow: Int?
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -16,7 +13,8 @@ class TRManageItemsViewController: UIViewController, UITableViewDataSource, UITa
         let addButton = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: "addItem")
         navigationItem.rightBarButtonItem = addButton
         navigationController?.navigationBar.tintColor = UIColor.TRBabyBlue()
-        itemsTableView.dataSource = self
+        itemsPresenter = TRManageItemsPresenter(itemsModel: self.itemsModel, itemsTableView: self.itemsTableView)
+        itemsTableView.dataSource = itemsPresenter
         itemsTableView.delegate = self
         NSNotificationCenter.defaultCenter().addObserver(self, selector: "reloadTable", name: "newItem", object: nil)
     }
@@ -49,59 +47,6 @@ class TRManageItemsViewController: UIViewController, UITableViewDataSource, UITa
     func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         selectedRow = indexPath.row
         performSegueWithIdentifier("showEditItemViewController", sender: nil)
-    }
-    
-    // MARK: UITableViewDataSource
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemsModel.allItems.count
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell: TRManageItemsTableViewCell = tableView.dequeueReusableCellWithIdentifier("items") as! TRManageItemsTableViewCell
-        if (indexPath.row == 0) {
-            cell.topBorder.hidden = false
-            cell.bottomBorder.hidden = true
-        } else if (indexPath.row == (itemsModel.allItems.count - 1)) {
-            cell.topBorder.hidden = true
-            cell.bottomBorder.hidden = false
-        } else {
-            cell.topBorder.hidden = true
-            cell.bottomBorder.hidden = true
-        }
-        let name: String = itemsModel.allItems[indexPath.row].name
-        cell.setSettingNameWith(name)
-        cell.toggleSwitch.tag = indexPath.row
-        cell.toggleSwitch.on = itemsModel.allItems[indexPath.row].activated
-        cell.manageItemsTableViewCellDelegate = self
-        return cell
-    }
-    
-    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-        if indexPath.row <= TRTrackableItems.allItems.count {
-            return false
-        } else {
-            return true
-        }
-    }
-    
-    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
-        if editingStyle == .Delete {
-            itemsModel.deleteItemAtIndex(indexPath.row)
-            tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        }
-    }
-    
-    // MARK: TRManageItemsTableViewCellDelegate
-    
-    func toggleSwitchChangedValueAtRow(row: Int) {
-        let indexPath = NSIndexPath(forRow: row, inSection: 0)
-        let cell = itemsTableView.cellForRowAtIndexPath(indexPath) as! TRManageItemsTableViewCell
-            itemsModel.updateItemsActiveStatusAtIndex(row, activeStatus: cell.toggleSwitch.on)
     }
     
 }
