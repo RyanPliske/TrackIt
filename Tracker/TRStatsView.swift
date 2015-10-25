d7641a65456340738bcf4c77c41626b749d4dd92
Ryan Pliske <ryan.pliske@monsanto.com>
2015-10-25 10:52:43 -0500

    Got rid of old calendar

--- Tracker/TRStatsView.swift
+++ Tracker/TRStatsView.swift
@@ -3,28 +3,25 @@ import JTCalendar
 
 class TRStatsView: UIView {
     
-    private let calendarContentView: JTHorizontalCalendarView
     let calendarManager: JTCalendarManager
+    private let contentView = UIView()
     
     init(frame: CGRect, _calendarManager: JTCalendarManager) {
         calendarManager = _calendarManager
-        calendarContentView = JTHorizontalCalendarView(frame: frame)
         super.init(frame: frame)
-        addSubview(calendarContentView)
-        calendarManager.contentView = calendarContentView
-        calendarManager.setDate(NSDate())
+        contentView.backgroundColor = UIColor.greenColor()
+        addSubview(contentView)
     }
 
     required init?(coder aDecoder: NSCoder) {
         calendarManager = JTCalendarManager()
-        calendarContentView = JTHorizontalCalendarView()
         super.init(coder: aDecoder)
     }
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        let calendarContentSize = CGSize(width: CGRectGetWidth(self.bounds), height: CGRectGetHeight(self.bounds) - CGRectGetHeight(self.bounds))
-        calendarContentView.frame = CGRectMake(0, 0, calendarContentSize.width, CGRectGetHeight(self.bounds))
+        let calendarContentSize = CGSize(width: CGRectGetWidth(self.bounds), height: CGRectGetHeight(self.bounds))
+        contentView.frame = CGRectMake(0, 0, calendarContentSize.width, calendarContentSize.height)
     }
     
 }
